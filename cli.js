d01af71a6378116de37097d381435caf941e4002
Andrea Parodi <andrea@parro.it>
2016-11-09 11:04:46 +0100

    cli

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+'use strict';
+
+const packageName = process.argv.slice(2)[0];
+const npmExists = require('.');
+
+if (packageName) {
+	npmExists(packageName).then(exists => {
+		if (exists) {
+			process.stdout.write(`Package ${packageName} is already registered.`);
+			process.exit(0);
+			return;
+		}
+		process.stdout.write(`Package ${packageName} is available.`);
+		process.exit(1);
+	});
+} else {
+	process.stderr.write('Usage: npm-exists <package name>\n\n');
+}
